2bbfeac82cd4a002a2d4c88d743a542daa38d3d6
Artem Slinkin <slartus@gmail.com>
2024-03-06 20:00:20 +0500

    wait message

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/AuthScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/AuthScreen.kt
@@ -1,10 +1,16 @@
 package ru.slartus.boostbuddy.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ru.slartus.boostbuddy.widgets.WebView
@@ -12,11 +18,18 @@ import ru.slartus.boostbuddy.components.AuthComponent
 
 @Composable
 fun AuthScreen(component: AuthComponent) {
-    Column {
-        Text(
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
             modifier = Modifier.fillMaxWidth().padding(16.dp),
-            text = "Авторизация"
-        )
+            verticalAlignment = CenterVertically
+        ) {
+            CircularProgressIndicator(modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = "Ожидание авторизационной куки"
+            )
+        }
+
         WebView("https://boosty.to", onCookieChange = component::onCookiesChanged)
     }
 }
