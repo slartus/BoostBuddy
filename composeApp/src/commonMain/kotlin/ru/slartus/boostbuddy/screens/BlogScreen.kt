21fa800334b18633807ca9893e2da54066560db9
Artem Slinkin <slartus@gmail.com>
2024-03-07 16:24:57 +0500

    min preview height

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -179,11 +181,14 @@ private fun PostView(post: Post, onClick: () -> Unit) {
         Spacer(modifier = Modifier.size(16.dp))
 
         if (post.previewUrl != null) {
-            Image(
-                modifier = Modifier.widthIn(max = 640.dp, min = 300.dp).fillMaxWidth().wrapContentHeight(),
-                painter = rememberImagePainter(post.previewUrl),
-                contentDescription = "preview",
-            )
+            Box(modifier = Modifier.heightIn(min = 200.dp)) {
+                Image(
+                    modifier = Modifier.widthIn(max = 640.dp).fillMaxWidth()
+                        .wrapContentHeight(),
+                    painter = rememberImagePainter(post.previewUrl),
+                    contentDescription = "preview",
+                )
+            }
         }
     }
 }
