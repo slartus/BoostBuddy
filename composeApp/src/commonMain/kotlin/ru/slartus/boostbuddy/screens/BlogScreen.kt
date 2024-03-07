1c11457ea7e0c13c2fbfcaf6c69b1f621e383205
Artem Slinkin <slartus@gmail.com>
2024-03-07 16:06:12 +0500

    redesign

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -32,10 +33,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
@@ -70,7 +69,10 @@ fun BlogScreen(component: BlogComponent) {
             )
         },
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            horizontalAlignment = CenterHorizontally
+        ) {
             when (val progressState = state.progressProgressState) {
                 is BlogViewState.ProgressState.Error -> Text(text = progressState.description)
                 BlogViewState.ProgressState.Init,
@@ -165,7 +167,8 @@ private fun PostView(post: Post, onClick: () -> Unit) {
     Column(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.primaryContainer)
-            .clickable { onClick() }.padding(16.dp)
+            .clickable { onClick() }.padding(16.dp),
+        horizontalAlignment = CenterHorizontally
     ) {
         Text(
             modifier = Modifier.fillMaxWidth(),
@@ -177,7 +180,7 @@ private fun PostView(post: Post, onClick: () -> Unit) {
 
         if (post.previewUrl != null) {
             Image(
-                modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+                modifier = Modifier.widthIn(max = 640.dp, min = 300.dp).fillMaxWidth().wrapContentHeight(),
                 painter = rememberImagePainter(post.previewUrl),
                 contentDescription = "preview",
             )
