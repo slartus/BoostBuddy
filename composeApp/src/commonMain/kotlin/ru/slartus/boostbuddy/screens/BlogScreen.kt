44420bde089ff918c9a2bbf0602094c705da5d00
Artem Slinkin <slartus@gmail.com>
2024-03-07 14:08:26 +0500

    refactoring

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/screens/BlogScreen.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
