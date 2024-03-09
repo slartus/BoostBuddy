e68bb7d313b760a2ebbcb3434cb5bc8a5c97ec4b
Artem Slinkin <slartus@gmail.com>
2024-03-09 11:14:31 +0500

    play icon fo video

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/ui/screens/blog/PostDataView.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/ui/screens/blog/PostDataView.kt
@@ -1,16 +1,25 @@
 package ru.slartus.boostbuddy.ui.screens.blog
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.seiko.imageloader.rememberImagePainter
@@ -128,5 +137,22 @@ private fun PostDataOkVideoView(
             painter = rememberImagePainter(postData.previewUrl),
             contentDescription = "preview",
         )
+
+        Box(
+            modifier = Modifier
+                .size(68.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.scrim.copy(alpha = 0.5f),
+                    shape = CircleShape
+                )
+                .align(Alignment.Center)
+                .padding(4.dp)
+        ) {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                imageVector = Icons.Filled.PlayArrow,
+                contentDescription = "Play video icon"
+            )
+        }
     }
 }
