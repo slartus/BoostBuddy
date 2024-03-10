666bb8b28414d74e65db54d55157f53b671eacc6
Artem Slinkin <slartus@gmail.com>
2024-03-10 19:22:48 +0500

    ios player

--- composeApp/src/iosMain/kotlin/ru/slartus/boostbuddy/ui/widgets/VideoPlayer.kt
+++ composeApp/src/iosMain/kotlin/ru/slartus/boostbuddy/ui/widgets/VideoPlayer.kt
@@ -1,9 +1,24 @@
 package ru.slartus.boostbuddy.ui.widgets
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.UIKitView
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.AVFoundation.AVPlayer
+import platform.AVFoundation.AVPlayerLayer
+import platform.AVFoundation.play
+import platform.AVKit.AVPlayerViewController
+import platform.CoreGraphics.CGRect
+import platform.Foundation.NSURL
+import platform.QuartzCore.CATransaction
+import platform.QuartzCore.kCATransactionDisableActions
+import platform.UIKit.UIView
 import ru.slartus.boostbuddy.components.video.VideoState
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun VideoPlayer(
     vid: String,
@@ -14,5 +29,35 @@ actual fun VideoPlayer(
     onContentPositionChange: (Long) -> Unit,
     onStopClick: () -> Unit
 ) {
-    Text(text = "Not implemented")
+
+    val player = remember { AVPlayer(uRL = NSURL.URLWithString(url)!!) }
+    val playerLayer = remember { AVPlayerLayer() }
+    val avPlayerViewController = remember { AVPlayerViewController() }
+    avPlayerViewController.player = player
+    avPlayerViewController.showsPlaybackControls = true
+
+    playerLayer.player = player
+    UIKitView(
+        modifier = Modifier.fillMaxSize(),
+        factory = {
+            val playerContainer = UIView()
+            playerContainer.addSubview(avPlayerViewController.view)
+            // Return the playerContainer as the root UIView
+            playerContainer.apply {
+
+            }
+        },
+        onResize = { view: UIView, rect: CValue<CGRect> ->
+            CATransaction.begin()
+            CATransaction.setValue(true, kCATransactionDisableActions)
+            view.layer.setFrame(rect)
+            playerLayer.setFrame(rect)
+            avPlayerViewController.view.layer.frame = rect
+            CATransaction.commit()
+        },
+        update = { view ->
+            player.play()
+            avPlayerViewController.player!!.play()
+        }
+    )
 }
