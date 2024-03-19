6a0ac5f4574929c656f45bf381db41b938dab585
Artem Slinkin <slartus@gmail.com>
2024-03-19 18:47:52 +0500

    c51

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -96,7 +96,7 @@ android {
         targetSdk = 34
 
         applicationId = "ru.slartus.boostbuddy"
-        versionCode = 50
+        versionCode = 51
         versionName = "1.0.5"
     }
     sourceSets["main"].apply {
