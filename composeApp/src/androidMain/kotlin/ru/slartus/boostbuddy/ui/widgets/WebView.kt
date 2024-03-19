153a8f1a1dc90520cc8ee047984f241c84f24b50
Artem Slinkin <slartus@gmail.com>
2024-03-19 21:25:00 +0500

    webview atv buttons

--- composeApp/src/androidMain/kotlin/ru/slartus/boostbuddy/ui/widgets/WebView.kt
+++ composeApp/src/androidMain/kotlin/ru/slartus/boostbuddy/ui/widgets/WebView.kt
@@ -9,28 +9,65 @@ import android.webkit.CookieManager
 import android.webkit.HttpAuthHandler
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
 import ru.slartus.boostbuddy.data.ktor.USER_AGENT
+import ru.slartus.boostbuddy.ui.common.BackHandlerEffect
+import ru.slartus.boostbuddy.ui.common.LocalPlatformConfiguration
+import ru.slartus.boostbuddy.utils.Platform
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlin.time.Duration.Companion.seconds
 
 
 @Composable
 actual fun WebView(url: String, clickCoors: Offset?, onCookieChange: (String) -> Unit) {
+    val isAtv = LocalPlatformConfiguration.current.platform == Platform.AndroidTV
     val scope = rememberCoroutineScope()
-
+    var buttons by remember { mutableStateOf<ImmutableList<HtmlButton>>(persistentListOf()) }
     val context = LocalContext.current
+    var buttonsJob: Job by remember { mutableStateOf(Job(null)) }
+    var webViewFocused by remember { mutableStateOf(false) }
     val webView = remember {
         WebView(context).apply {
+            this.setOnFocusChangeListener { view, b ->
+                webViewFocused = b
+            }
             settings.apply {
                 javaScriptEnabled = true
                 userAgentString = USER_AGENT
@@ -62,6 +99,17 @@ actual fun WebView(url: String, clickCoors: Offset?, onCookieChange: (String) ->
                 override fun onLoadResource(view: WebView?, url: String?) {
                     if (url != null)
                         onCookieChange(CookieManager.getInstance().getCookie(url).orEmpty())
+                    if (isAtv) {
+                        buttonsJob.cancel()
+                        buttonsJob = scope.launch(SupervisorJob()) {
+                            delay(1.seconds)
+                            buttons =
+                                view?.getAllButtons().orEmpty()
+                                    .filter { !it.text.isNullOrEmpty() }
+                                    .distinctBy { it.id }
+                                    .toImmutableList()
+                        }
+                    }
                 }
 
                 override fun onPageFinished(view: WebView?, currentUrl: String?) {
@@ -76,12 +124,48 @@ actual fun WebView(url: String, clickCoors: Offset?, onCookieChange: (String) ->
         }
     }
 
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = {
-            webView
+    BackHandlerEffect(enabled = webViewFocused) {
+        if (webViewFocused)
+            webView.clearFocus()
+    }
+
+    Column {
+        if (isAtv) {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                items(buttons, key = { it.id }) { button ->
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.primaryContainer)
+                            .clickable {
+                                webView.evaluateJavascript(
+                                    "document.getElementById(\"${button.id}\").click();",
+                                    null
+                                )
+                            }
+                            .padding(4.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = button.text.orEmpty(),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
+                }
+            }
         }
-    )
+        AndroidView(
+            modifier = Modifier
+                .weight(1f),
+            factory = {
+                webView
+            }
+        )
+    }
     LaunchedEffect(clickCoors) {
         if (clickCoors != null) {
             webView.simulateClick(scope, clickCoors.x, clickCoors.y)
@@ -94,18 +178,43 @@ actual fun WebView(url: String, clickCoors: Offset?, onCookieChange: (String) ->
 
 private fun WebView.injectCSS() {
     try {
-       val style = ":focus { border: 2px solid red; }"
+        val style = ":focus { border: 2px solid red; }"
         val script =
-                "var parent = document.getElementsByTagName('head').item(0);\n" +
-                "var style = document.createElement('style');\n" +
-                "style.type = 'text/css';\n" +
-                "style.innerHTML = \"$style\";\n" +
-                "parent.appendChild(style)"
+            "var parent = document.getElementsByTagName('head').item(0);\n" +
+                    "var style = document.createElement('style');\n" +
+                    "style.type = 'text/css';\n" +
+                    "style.innerHTML = \"$style\";\n" +
+                    "parent.appendChild(style)"
         evaluateJavascript(script, null)
     } catch (e: Exception) {
         e.printStackTrace()
     }
 }
+
+private suspend fun WebView.getAllButtons(): List<HtmlButton> = suspendCoroutine { continuation ->
+    val script =
+        "Array.from(document.querySelectorAll(\"button, [role='button']\")).forEach(b=>b.id = b.id||(Date.now().toString(36) + Math.random().toString(36).substr(2)));" +
+                "return Array.from(document.querySelectorAll(\"button, [role='button']\")).map(function(b){return {id: b.id, text: b.innerText||b.getAttribute('data-provider')}});"
+
+    val functionScript = "(function() { $script; })();"
+    val json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+    evaluateJavascript(functionScript) { result ->
+        runCatching {
+            continuation.resume(
+                json.decodeFromString(
+                    ListSerializer(HtmlButton.serializer()),
+                    result
+                )
+            )
+        }.onFailure { continuation.resumeWithException(it) }
+    }
+}
+
+@Serializable
+private data class HtmlButton(val id: String, val text: String?)
 
 private fun WebView.simulateClick(coroutineScope: CoroutineScope, x: Float, y: Float) {
     coroutineScope.launch {
