6c140dfa5e9f4066233ffa2dffdab00094111b7a
Артём Слинкин <Artyom.Slinkin@leroymerlin.ru>
2025-07-17 09:27:16 +0500

    text item with icon

--- composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/ui/screens/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/slartus/boostbuddy/ui/screens/SettingsScreen.kt
@@ -3,15 +3,24 @@ package ru.slartus.boostbuddy.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.Attachment
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import ru.slartus.boostbuddy.components.settings.SettingsComponent
@@ -32,6 +41,7 @@ internal fun SettingsScreen(component: SettingsComponent) {
                 }
             )
             TextItem(
+                icon = Icons.Default.AttachMoney,
                 text = "Поддержать проект",
                 onClick = {
                     component.onDonateClicked()
@@ -48,6 +58,7 @@ internal fun SettingsScreen(component: SettingsComponent) {
 
             if (state.appSettings.debugLog) {
                 TextItem(
+                    icon = Icons.Default.Attachment,
                     text = "Отправить лог",
                     onClick = {
                         component.onSendLogClicked()
@@ -56,6 +67,7 @@ internal fun SettingsScreen(component: SettingsComponent) {
             }
 
             TextItem(
+                icon = Icons.Default.Info,
                 text = "Версия программы: ${platformConfiguration.appVersion}",
                 onClick = {
                     component.onVersionClicked()
@@ -67,14 +79,25 @@ internal fun SettingsScreen(component: SettingsComponent) {
 
 @Composable
 private fun TextItem(
+    icon: ImageVector,
     text: String,
     onClick: () -> Unit = {}
 ) {
-    Text(
-        modifier = Modifier.fillMaxWidth().clickable { onClick() }.padding(16.dp),
-        text = text,
-        style = MaterialTheme.typography.bodyMedium
-    )
+    Row(
+        Modifier.fillMaxWidth().clickable { onClick() }.padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            imageVector = icon,
+            contentDescription = "Icon"
+        )
+        Spacer(Modifier.width(12.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 @Composable
