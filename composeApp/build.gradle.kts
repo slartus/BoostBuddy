19266a591da38a1039139198ea8588a34b4202b9
Артём Слинкин <Artyom.Slinkin@leroymerlin.ru>
2025-12-08 14:32:38 +0500

    1.8.3c79

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -102,8 +102,8 @@ android {
         targetSdk = 34
 
         applicationId = "ru.slartus.boostbuddy"
-        versionCode = 78
-        versionName = "1.8.2"
+        versionCode = 79
+        versionName = "1.8.3"
     }
     sourceSets["main"].apply {
         manifest.srcFile("src/androidMain/AndroidManifest.xml")
